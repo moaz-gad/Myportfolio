80c3b27f72e0f81f3b595a50875449fce001e1c7
moaz <moaz.a.gad@gmail.com>
2023-03-13 12:19:21 +0200

    Updates

--- precache-manifest.206b35aaccc5443343cce7df67e31a8f.js
+++ precache-manifest.206b35aaccc5443343cce7df67e31a8f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "06889687cc5735528fba",
-    "url": "/Myportfolio/static/css/main.a89700aa.chunk.css"
+    "revision": "6cdda45a1af2a2b2d0f2",
+    "url": "/Myportfolio/static/css/main.4a71a91b.chunk.css"
   },
   {
-    "revision": "06889687cc5735528fba",
-    "url": "/Myportfolio/static/js/main.06889687.chunk.js"
+    "revision": "6cdda45a1af2a2b2d0f2",
+    "url": "/Myportfolio/static/js/main.6cdda45a.chunk.js"
   },
   {
     "revision": "0649babc04ce7d8fc631",
@@ -68,20 +68,12 @@ self.__precacheManifest = [
     "url": "/Myportfolio/static/media/Agustina.21f233e1.woff"
   },
   {
-    "revision": "ee6539921d713482b8ccd4d0d23961bb",
-    "url": "/Myportfolio/static/media/Montserrat-Regular.ee653992.ttf"
-  },
-  {
-    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
-    "url": "/Myportfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
-  },
-  {
     "revision": "90773b6158663ab0fe78b32680733677",
     "url": "/Myportfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
   },
   {
-    "revision": "4457817ac2b9993c65e81aa05828fe9c",
-    "url": "/Myportfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Myportfolio/static/media/Montserrat-Regular.ee653992.ttf"
   },
   {
     "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
@@ -92,20 +84,24 @@ self.__precacheManifest = [
     "url": "/Myportfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
   },
   {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/Myportfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/Myportfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
     "revision": "8d57e4014b18edef070d285746485115",
     "url": "/Myportfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
   },
   {
-    "revision": "cccc9d29470e879e40eb70249d9a2705",
-    "url": "/Myportfolio/static/media/fa-brands-400.cccc9d29.woff2"
-  },
-  {
-    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
-    "url": "/Myportfolio/static/media/fa-brands-400.c5e0f14f.woff"
-  },
-  {
     "revision": "5063b105c7646c8043d58c5289f02cca",
     "url": "/Myportfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/Myportfolio/static/media/fa-brands-400.cccc9d29.woff2"
   },
   {
     "revision": "06147b6cd88c7346cecd1edd060cd5de",
@@ -116,8 +112,8 @@ self.__precacheManifest = [
     "url": "/Myportfolio/static/media/fa-regular-400.f5f2566b.woff2"
   },
   {
-    "revision": "65b286af947c0d982ca01b40e1fcab38",
-    "url": "/Myportfolio/static/media/fa-regular-400.65b286af.ttf"
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/Myportfolio/static/media/fa-brands-400.c5e0f14f.woff"
   },
   {
     "revision": "c1a866ec0e04a5e1915b41fcf261457c",
@@ -128,16 +124,20 @@ self.__precacheManifest = [
     "url": "/Myportfolio/static/media/fa-regular-400.c4f508e7.woff"
   },
   {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/Myportfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/Myportfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
     "revision": "9443c1e7fdf95d739af3f0901224f90a",
     "url": "/Myportfolio/static/media/fa-regular-400.9443c1e7.svg"
   },
   {
     "revision": "333bae208dc363746961b234ff6c2500",
     "url": "/Myportfolio/static/media/fa-solid-900.333bae20.woff"
-  },
-  {
-    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
-    "url": "/Myportfolio/static/media/fa-solid-900.8e4a6dcc.eot"
   },
   {
     "revision": "44d537ab79f921fde5a28b2c1636f397",
@@ -188,7 +188,7 @@ self.__precacheManifest = [
     "url": "/Myportfolio/static/media/maryland_logo.225c8df3.svg"
   },
   {
-    "revision": "d3e4d8c79d8ccdc835140dc3489df81c",
+    "revision": "97ce6057ffa831d8215562045c86f65a",
     "url": "/Myportfolio/index.html"
   }
 ];
