919142355d24167846783d77393e9148e8486f53
moaz <moaz.a.gad@gmail.com>
2023-04-03 05:45:30 +0200

    education

--- src/components/degreeCard/DegreeCard.js
+++ src/components/degreeCard/DegreeCard.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Fade, Flip } from "react-reveal"
 import "./DegreeCard.css"
 
 function DegreeCard(props) {
@@ -8,49 +7,36 @@ function DegreeCard(props) {
 
   return (
     <div className="degree-card">
-      {degree.logo_path && (
-        <Flip left duration={2000}>
-          <div className="card-img">
-            <img
-              style={{
-                maxWidth: "100%",
-                maxHeight: "100%",
-                objectFit: "contain",
-              }}
-              src={require(`../../assets/images/${degree.logo_path}`).default}
-              alt={degree.alt_name}
-            />
-          </div>
-        </Flip>
-      )}
-      <Fade right duration={2000} distance="40px">
-        <div
-          className="card-body"
-          style={{ backgroundColor: theme.themeColor }}
-        >
-          <div
-            className="card-header"
-            style={{ backgroundColor: theme.accentColor }}
-          >
-            <div className="card-header-title">
-              <span className="card-title">{degree.title}</span>
+      <div className="card-body">
+        <div className="card-header">
+          <div className="card-header">
+            <div className="university">
+              {degree.logo_path && (
+                <div className="card-img">
+                  <img
+                    src={require(`../../assets/images/${degree.logo_path}`)}
+                    alt={degree.alt_name}
+                  />
+                </div>
+              )}
+              <h1 className="card-title">{degree.title}</h1>
             </div>
             <div className="card-header-duration">
               <span className="card-duration">{degree.duration}</span>
             </div>
           </div>
-          <div className="card-content" style={{ color: theme.text }}>
-            <span className="card-subtitle">{degree.subtitle}</span>
-            {degree.descriptions.map((sentence, index) => {
-              return (
-                <p key={"degree-" + index} className="content-list">
-                  {sentence}
-                </p>
-              )
-            })}
-          </div>
         </div>
-      </Fade>
+        <div className="card-content">
+          <span className="card-subtitle">{degree.subtitle}</span>
+          {degree.descriptions.map((sentence, index) => {
+            return (
+              <p key={"degree-" + index} className="content-list">
+                {sentence}
+              </p>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
